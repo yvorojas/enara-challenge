33680ab78559671972570a630951f15f04c3548e
Yvo Rojas <yrojasv@ripley.com>
2021-10-07 18:03:45 -0300

    [@yvorojas] adds integration test for stop endpoint

--- test/integration/tracker.test.ts
+++ test/integration/tracker.test.ts
@@ -23,4 +23,18 @@ describe('Tracker integration tests', () => {
       message: `started new segment for project ${project}`,
     });
   });
+
+  it('should get successfull message when call to stop endpoint', async () => {
+    const project = 'project';
+    await request.post(`/api/v1/tracker/${project}/start`).expect(201);
+
+    const res = await request
+      .post(`/api/v1/tracker/${project}/stop`)
+      .expect(201);
+
+    expect(res.body.message).toStrictEqual(
+      `stopped new segment for project ${project}`,
+    );
+    expect(res.body.duration).not.toBeNull();
+  });
 });
