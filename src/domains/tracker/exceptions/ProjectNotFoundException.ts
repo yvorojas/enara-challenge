e8bf548eff3f7a8b00e269dfb0219774785bee26
Yvo Rojas <yrojasv@ripley.com>
2021-10-08 00:54:05 -0300

    [@yvorojas] changes project not found message, adds sad path in OAS

--- src/domains/tracker/exceptions/ProjectNotFoundException.ts
+++ src/domains/tracker/exceptions/ProjectNotFoundException.ts
@@ -1,7 +1,7 @@
 import BaseError from '../../../infrastructure/common/exceptions/BaseError';
 const code = 'PROJECT_NOT_FOUND';
 const name = 'ProjectNotFoundException';
-const baseMessage = 'Project to stop segment not found';
+const baseMessage = 'Project not found';
 const status = 404;
 export default class ProjectNotFoundException extends BaseError {
   constructor(project) {
